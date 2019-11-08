44a2ff8ded6d3464dd4a510110960eef2efa6188
Lucas Evangelista <luquinha207@hotmail.com>
2019-11-08 14:49:02 -0300

    Inserção do SliverbarApp e o card

--- lib/Paginas/add.dart
+++ lib/Paginas/add.dart
@@ -9,11 +9,54 @@ class _AddState extends State<Add> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Add'),
-      ),
-      body: (
-        Text('Olá galeraaaaaaaaaaa')
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 200.0,
+            floating: false,
+            pinned: false,
+            snap: false,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.network(
+                "https://media.metrolatam.com/2019/11/06/tesssouthhiresaz-96cb6d24540428907540ba113454f440-1200x600.jpg",
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SliverFillRemaining(
+            child: Align(
+              alignment: Alignment(2, 0.2),
+              child: Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const ListTile(
+                      leading: Icon(Icons.album),
+                      title: Text('The Enchanted Nightingale'),
+                      subtitle:
+                          Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+                    ),
+                    ButtonTheme.bar(
+                      // make buttons use the appropriate styles for cards
+                      child: ButtonBar(
+                        children: <Widget>[
+                          FlatButton(
+                            child: const Text('BUY TICKETS'),
+                            onPressed: () {/* ... */},
+                          ),
+                          FlatButton(
+                            child: const Text('LISTEN'),
+                            onPressed: () {/* ... */},
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.grey,
+        primaryColor: Colors.black,
       ),
       home: Home() ,
     );
